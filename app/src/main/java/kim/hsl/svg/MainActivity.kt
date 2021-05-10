ec29c0ca82a4a161348f884e40d62815164f3dbc
han1202012 <octopus_truth@163.com>
2021-05-10 20:42:40 +0800

    update

--- app/src/main/java/kim/hsl/svg/MainActivity.kt
+++ app/src/main/java/kim/hsl/svg/MainActivity.kt
@@ -1,11 +1,23 @@
 package kim.hsl.svg
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.drawable.DrawableCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // 获取图片
+        var drawable: Drawable = resources.getDrawable(R.drawable.ic_plane)
+        // 设置图片为绿色
+        DrawableCompat.setTint(drawable, Color.GREEN)
+
+        // 设置绿色图片
+        findViewById<ImageView>(R.id.first_image).setImageDrawable(drawable)
     }
 }
